98e4775781d98a8fd65e419fa91d87d4b4ad7051
Butko Denis <butko.dv@gmail.com>
2024-04-24 09:47:31 +0300

    Add calculateTeamFinanceReport function and sample data

---
+++ calculateTeamFinanceReport.js
@@ -0,0 +1,15 @@
+function calculateTeamFinanceReport(salaries, team) {
+  // the implementation of your function here
+}
+
+const salaries1 = {
+  Manager: { salary: 1000, tax: '10%' },
+  Designer: { salary: 600, tax: '30%' },
+  Artist: { salary: 1500, tax: '15%' },
+};
+const team1 = [
+  { name: 'Misha', specialization: 'Manager' },
+  { name: 'Max', specialization: 'Designer' },
+  { name: 'Vova', specialization: 'Designer' },
+  { name: 'Leo', specialization: 'Artist' },
+];
